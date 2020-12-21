4148c381676d99b50e2fc666e96117585458ff63
Avi Bauer <bauerac@bc.edu>
2020-12-21 14:04:42 -0500

    Removed link to ATR appt calendar

--- library-home-hours-2.js
+++ library-home-hours-2.js
@@ -16,7 +16,8 @@ const callback = function(mutationsList, observer) {
 
             //add links to building and ATR hours
             $('tr.loc5008').append('<td class="link"><a href="library/using/hours.html"><span class="link-text">Detailed Hours</span></a></td>');
-            $('tr.dep14722').append('<td class="link"><a href="mailto:atrinbox@bc.edu"><span class="link-text">Email ATR</span></a></td><td class="link"><a href="https://calendar.google.com/calendar/selfsched?sstoken=UU90ckhhSmMyUHJrfGRlZmF1bHR8YmUxYzIzZGM1MjAzMWEyYjE5MWZkYWZmNjM2NTYyZTc"><span class="link-text">Make an Appointment</span></a></td>');
+            //$('tr.dep14722').append('<td class="link"><a href="mailto:atrinbox@bc.edu"><span class="link-text">Email ATR</span></a></td><td class="link"><a href="https://calendar.google.com/calendar/selfsched?sstoken=UU90ckhhSmMyUHJrfGRlZmF1bHR8YmUxYzIzZGM1MjAzMWEyYjE5MWZkYWZmNjM2NTYyZTc"><span class="link-text">Make an Appointment</span></a></td>'); //email ATR and appointment calendar
+            $('tr.dep14722').append('<td class="link"><a href="mailto:atrinbox@bc.edu"><span class="link-text">Email ATR</span></a></td>'); //just email, no appointment calendar
             $('tr.dep5014').append('<td class="link"><a href="https://lawguides.bc.edu/educationandreference"><span class="link-text">Get Research Help</span></a></td>')
             //Only run the function once and disconnect the observer once it has run
             observer.disconnect();
