41e121f72df21f344e9150fd53c92a589c78f520
Avi Bauer <bauerac@bc.edu>
2022-01-28 08:52:24 -0500

    Updated library chat widget image

--- library-home-hours-2.js
+++ library-home-hours-2.js
@@ -18,10 +18,9 @@ const callback = function(mutationsList, observer) {
             $('tr.loc5008').append('<td class="link"><a href="library/using/hours.html"><span class="link-text">Detailed Hours</span></a></td>');
             $('tr.dep14722').append('<td class="link"><a href="mailto:atrinbox@bc.edu"><span class="link-text">Email ATR</span></a></td><td class="link"><a href="https://calendar.google.com/calendar/selfsched?sstoken=UU90ckhhSmMyUHJrfGRlZmF1bHR8YmUxYzIzZGM1MjAzMWEyYjE5MWZkYWZmNjM2NTYyZTc"><span class="link-text">Make an Appointment</span></a></td>'); //email ATR and appointment calendar
             //$('tr.dep14722').append('<td class="link"><a href="mailto:atrinbox@bc.edu"><span class="link-text">Email ATR</span></a></td>'); //just email, no appointment calendar
-            $('tr.dep5014').append('<td class="link"><a href="https://lawguides.bc.edu/educationandreference"><span class="link-text">Get Research Help</span></a></td>')
+            $('tr.dep5014').append('<td class="link"><div id="libchat_89869b0c11c3b20ddcf044f4db5e59e2"></div><a href="https://lawguides.bc.edu/educationandreference"><span class="link-text">Get Research Help</span></a></td>')
             //Only run the function once and disconnect the observer once it has run
             observer.disconnect();
-
         }
     }
 };
