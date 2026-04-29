7b6d0c8e572403b30bb5193f744390c4c625d334
sengutengu <seungkim.h@gmail.com>
2026-04-29 11:18:40 -0400

    Fix hours bar overflow

--- js/hours.js
+++ js/hours.js
@@ -2,7 +2,7 @@ const thLibraryHours = document.querySelector("tr.hours-library > th");
 const thTechSupport = document.querySelector("tr.dep14722 > th");
 const thResearchHelp = document.querySelector("tr.dep5014 > th");
 if (thLibraryHours) {
-  thLibraryHours.innerHTML = `<a href="https://www.bc.edu/bc-web/schools/law/sites/students/library/using/hours.html" target="_blank">Library Building Hours</a>`;
+  thLibraryHours.innerHTML = `<a href="https://www.bc.edu/bc-web/schools/law/sites/students/library/using/hours.html" target="_blank">Library Hours</a>`;
 }
 
 if (thLibraryHours) {
